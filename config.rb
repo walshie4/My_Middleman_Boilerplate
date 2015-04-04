9b0adfbb87dbbdf2675dcd7eb8c523ebd54fb57b
Adam Walsh <adamwalsh14@gmail.com>
2015-04-04 18:43:04 -0400

    init

---
+++ config.rb
@@ -0,0 +1,37 @@
+###
+#HAML
+###
+set :haml, { :ugly => true, :format => :html5 }
+
+###
+# Page options, layouts, aliases and proxies
+###
+page "", :layout => "base"
+
+# Development-specific configuration
+configure :development do
+  activate :livereload
+  activate :directory_indexes
+end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :build_dir, 'build'
+
+# Build-specific configuration
+configure :build do
+  # Minify CSS on build
+  activate :minify_css
+  # Minify Javascript on build
+  activate :minify_javascript
+  # Utilize gzip compression
+  activate :gzip
+  # Minify HTML on build
+  activate :minify_html
+  # Use directory indexes (no file extension in URL)
+  activate :directory_indexes
+end
